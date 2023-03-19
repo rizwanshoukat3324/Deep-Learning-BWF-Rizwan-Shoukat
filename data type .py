1b5e8f05c69a9b5b6f4484b1e3d3bba5dafa70a0
Rizwan Shoukat <111968550+rizwanshoukat3324@users.noreply.github.com>
2023-03-19 11:05:41 -0700

    Add files via upload

---
+++ data type .py
@@ -0,0 +1,33 @@
+#secondly we will look on Data types 
+# a data type is an attribute of data which tells the interpreter or compiler how the programmer intends to use the data.
+A=7
+B=7.1
+C=7+9j
+print (type(C))
+#above is an example of NUMERIC DATA TYPE for Int,float and complex data type
+
+
+name="Hello Rizwan shoukat"
+print(name)
+#above is an example of TEXT DATA TYPE for str
+
+
+Nameofmonths=["jan,feb,mar,april,may,june,july,aug,sept,oct,nov,dec"] 
+print(type(Nameofmonths))
+print(Nameofmonths)
+#Above is an example of seqnece type data for list
+
+
+nameofdays=("Monday,tuesday,wednesday,thursday,friday,saturday,sunday")
+type(nameofdays)
+print(type(nameofdays))
+print(nameofdays)
+#Above is an example of SEQUENCE TYPE DATA for Tuple
+
+
+set={1,2,1,2,3,4,5,6,7,8,9,9,9,}
+type(set)
+print(type(set))
+print(set)
+#Above is an example of SET DATA TYPE for set
+
---
+++ variable .py
@@ -0,0 +1,16 @@
+# Firstly i will explain about Variable, We explain it as
+#varible is a named storage location in memory that can hold a value.Variables are used to store data that is needed by a program, such as numbers, text, or other types of information.
+#For example,
+A=78
+B=5
+print(A+B)
+#where 'A' and 'B' we declare as a varible which holds numeric value
+#here another example
+name=("Rizwan shoukat")
+print(name)
+# where name is a variable
+#Another example
+var1="Say hello to Rizwan!\n"
+var2="  Hello Rizwan\n"
+var3="Thanks\n"
+print(var1+var2+var3)
